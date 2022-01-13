3f3fb7708c6ec86ce4221003ae5a7fede0fd81cb
Purple Professional <97543454+PurpleProfessional@users.noreply.github.com>
2022-01-13 14:45:53 -0500

    Update Discord Uninstall Tool.bat

--- Discord Uninstall Tool.bat
+++ Discord Uninstall Tool.bat
@@ -5,8 +5,6 @@ del /f /q C:\Users\%username%\AppData\Local\Discord
 del /f /q C:\Users\%username%\Desktop\Discord.exe
 del /f /q C:\Users\%username%\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Discord.exe
 del /f /q C:\ProgramData\SquirrelMachineInstalls\Discord.exe
-del /f /q C:\Users\%username%\AppData\Roaming\BetterDiscord
-del /f /q C:\Users\%username%\AppData\Roaming\BetterDiscordInstaller
 reg delete HKEY_CURRENT_USER\Software\Discord
-echo Discord and BetterDiscord Deleted!
+echo Discord Deleted!
 pause >nul
